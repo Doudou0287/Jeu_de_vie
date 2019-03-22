36c7a30afe367f5e41f9580f36edf298f46a37b6
s.alkebsi <sundus.alkebsi@etu.unistra.fr>
2019-03-22 15:16:56 +0100

    ajouter le repertoire src en ajoutant les fichiers .c dans ce repertoire

---
+++ src/grille.c
@@ -0,0 +1,84 @@
+/** 
+* \file grille.c
+* \brief  code pour les grilles  
+* \author sundus alkebsi
+*/ 
+
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "grille.h"
+
+void alloue_grille(int l, int c, grille *g) { 
+	int i,j;
+	g->nbl=l;
+	g->nbc=c;
+	g->cellules=malloc(l*sizeof(int*));
+	if(g->cellules==NULL)
+	{
+		exit(0);
+	}
+
+	for( i=0; i<l; i++ )
+	{
+		g->cellules[i]=malloc(c*sizeof(int));
+		if (g->cellules[i]==NULL)
+			exit(0);
+		for( j=0; j<c; j++)
+			set_morte(i,j,*g);	
+	}
+	
+
+ }
+
+void libere_grille(grille *g) {
+	int i;
+	for( i=0; i<g->nbl;i++){
+	free(g->cellules[i]);
+	}
+	free(g->cellules);
+}
+
+void init_grille_from_file(char const *const filename, grille *const g) {
+  FILE *pfile = fopen(filename, "r");
+  if (pfile == NULL) {
+    fprintf(stderr,
+            "Erreur (%s:%d): Lecture du fichier \"%s\" impossible (%s)\n",
+            __FILE__, __LINE__ - 4, filename, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  int l, c;
+  fscanf(pfile, "%d", &l);
+  fscanf(pfile, "%d", &c);
+
+  alloue_grille(l, c, g);
+	int n;
+  int vivantes = 0;
+  fscanf(pfile, "%d", &vivantes);
+  for ( n = 0; n < vivantes; ++n) {
+    fscanf(pfile, "%d", &l);
+    fscanf(pfile, "%d", &c);
+    set_vivante(l, c, *g);
+  }
+
+  fclose(pfile);
+  return;
+}
+
+void copie_grille(const grille *const gs, grille *const gd) {
+	int i,j;
+  assert(gs->nbl == gd->nbl && gs->nbc == gd->nbc);
+  for (i = 0; i < gs->nbl; ++i)
+    for ( j = 0; j < gs->nbc; ++j)
+      gd->cellules[i][j] = gs->cellules[i][j];
+}
+
+
+
+
+
---
+++ src/io.c
@@ -0,0 +1,106 @@
+/**
+ * \file io.c
+ * \brief code pour l'affichage
+ * \author sundus ALKEBSI
+ */
+
+
+
+#include "io.h"
+
+static void affiche_trait(int c) {
+  int i;
+  for (i = 0; i < c; ++i)
+    printf("|---");
+  printf("|\n");
+  return;
+}
+
+static void affiche_ligne(int ligne, const grille *g) {
+	int i; 
+ for ( i = 0; i < g->nbc; ++i) {
+    if (est_vivante(ligne, i, *g))
+      printf("| O ");
+    else
+      printf("|   ");
+  }
+  printf("|\n");
+  return;
+}
+
+void affiche_grille(const grille *g) {
+	int i; 
+ printf("\n");
+  affiche_trait(g->nbc);
+  for ( i = 0; i < g->nbl; ++i) {
+    affiche_ligne(i, g);
+    affiche_trait(g->nbc);
+  }
+  printf("\n");
+  return;
+}
+
+void efface_grille(const grille *g) { printf("\n\e[%dA", g->nbl * 2 + 5); }
+
+
+
+
+
+
+int (*compte_voisins_vivants) (int,int,grille)=compte_voisins_vivants_c;
+void debut_jeu(grille *g, grille *gc) {
+  int c = getchar();
+   int t=0;
+	int vieil=0;
+  while (c != 'q') { // touche 'q' pour quitter
+    switch (c) {
+    case '\n': { // touche "entree" pour évoluer
+      evolue(g, gc,vieil);
+      efface_grille(g);
+      affiche_grille(g);
+      t++;
+      printf("\e[2K");
+      printf("%d\t",t);
+
+      break;
+    }
+    case 'n' :
+    {
+	char nom[50];
+	printf("entrez le nom de grille\n");
+	scanf("%s",nom);
+	init_grille_from_file(nom,g);
+	alloue_grille((g->nbl),(g->nbc),gc);
+	t=0;
+	break;
+	} 
+    case 'c' :
+	{
+	if(compte_voisins_vivants==compte_voisins_vivants_c){
+			compte_voisins_vivants=compte_voisins_v_n_c;
+	}
+	else {
+		compte_voisins_vivants=compte_voisins_vivants_c;
+	  }
+	
+	break;
+	}
+     case 'v' :
+      {
+	if(vieil==0)
+	{
+		vieil=1;
+	}
+	else 
+		vieil=0;
+     }
+    default: { // touche non traitée
+      printf("\n\e[1A");
+      break;
+    }
+    }
+    c = getchar();
+  }
+  return;
+}
+
