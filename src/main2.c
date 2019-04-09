48074934f2fc7234cb5b5def4824cae8348a51fb
s.alkebsi <sundus.alkebsi@etu.unistra.fr>
2019-04-10 01:49:05 +0200

    ajoute un fichier main pour le mode graphique

---
+++ src/main2.c
@@ -0,0 +1,28 @@
+/**
+ * \file main2.c
+ * \brief le main programme pour le mode graphique
+ * \author Sundus alkebsi
+ */
+
+#include <stdio.h>
+#include "grille.h"
+#include "io.h"
+#include "jeu.h"
+
+
+int main (int argc, char ** argv) {
+	if (argc != 2 )
+	{
+		printf("usage : main <fichier grille>");
+		return 1;
+	}
+
+	grille g, gc;
+	init_grille_from_file(argv[1],&g);
+	alloue_grille (g.nbl, g.nbc, &gc);
+	graphique(&g, &gc);
+	libere_grille(&g);
+	libere_grille(&gc);
+	return 0;
+}
+
